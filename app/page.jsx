1452d468379af9fc3c734bc985a746c0e64bcb52
AbdurRahmanSalami <abdursalamioluwa@gmail.com>
2025-03-13 15:28:32 +0100

    'resume'

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Link href="/Abdur-RahmanSalamiCV.docx" download>
+              <Link href="/Abdur-RahmanSalamiCV.pdf" download target="_blank" rel="noopener noreferrer">
                 <Button
                   variant="outline"
                   size="lg"
